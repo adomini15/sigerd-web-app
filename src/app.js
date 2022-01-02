f76458c444b62abf5e101d3d43c5d0d15ca33c8a
adomini15 <adomini.edu@gmail.com>
2022-01-02 00:03:31 -0400

    Configuration changes applied

--- src/app.js
+++ src/app.js
@@ -1,106 +1,104 @@
 // imports
-import dotenv from 'dotenv';
-import fetch from 'node-fetch';
-import express from 'express'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
-import path from 'path'
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
-
 
 dotenv.config();
 
 // middlewares personalizados
-import login from './middlewares/Auth/Auth';
+import login from "./middlewares/Auth/Auth";
 
 // Configurando la utilidad CookieParser
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Gestión de los archivos públicos
-app.use(express.static(path.join(path.resolve(), 'src', 'public')))
+app.use(express.static(path.join(path.resolve(), "src", "public")));
 
 // Configurando el motor de vista
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Estableciendo la ruta a los archivos de vista
-app.set('views', path.join(path.resolve(), 'src', 'views'))
+app.set("views", path.join(path.resolve(), "src", "views"));
 
 // Utilizando morgan
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 // routes
 
-app.get('/', login, (req, res) => {
-  res.render('index', { title: 'Home' })
-})
+app.get("/", login, (req, res) => {
+	res.render("index", { title: "Home" });
+});
 
-app.get('/signup', (req, res) => {
-  res.render('signup', { title: 'Signup' })
-})
+app.get("/signup", (req, res) => {
+	res.render("signup", { title: "Signup" });
+});
 
-app.get('/login', (req, res) => {
-  res.render('login', { title: 'Login' })
-})
+app.get("/login", (req, res) => {
+	res.render("login", { title: "Login" });
+});
 
-app.get('/profile', login, (req, res) => {
-  res.render('profile', { title: 'profile' })
-})
+app.get("/profile", login, (req, res) => {
+	res.render("profile", { title: "profile" });
+});
 
-app.get('/enroll-student', login, (req, res) => {
-  res.render('enroll-student', { title: 'Enroll Student' })
-})
+app.get("/enroll-student", login, (req, res) => {
+	res.render("enroll-student", { title: "Enroll Student" });
+});
 
-app.get('/student/create', login, (req, res) => {
-  res.render('create-student', { title: 'New Student', student: null })
-})
+app.get("/student/create", login, (req, res) => {
+	res.render("create-student", { title: "New Student", student: null });
+});
 
-app.get('/student/:id/edit', login, async (req, res) => {
-  const endpoint = `http://localhost:3000/students/${req.params.id}`;
+app.get("/student/:id/edit", login, async (req, res) => {
+	const endpoint = `http://localhost:3000/students/${req.params.id}`;
 
-  try {
-    const response = await fetch(endpoint)
-    const student = await response.json()
+	try {
+		const response = await fetch(endpoint);
+		const student = await response.json();
 
-    if (student._id) {
-      res.render('edit-student', { title: 'Edit Student', student })
-    } else {
-      res.redirect("http://localhost:3001/")
-    }
-  } catch (error) {
-    console.log(error)
-  }
+		if (student._id) {
+			res.render("edit-student", { title: "Edit Student", student });
+		} else {
+			res.redirect("http://localhost:3001/");
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
 
-})
+app.get("/roll", login, async (req, res) => {
+	const endpoint = `http://localhost:3000/courses`;
 
-app.get('/roll', login, async (req, res) => {
-  const endpoint = `http://localhost:3000/courses`;
+	try {
+		const response = await fetch(endpoint);
+		const courses = await response.json();
 
-  try {
-    const response = await fetch(endpoint)
-    const courses = await response.json()
+		if (courses) {
+			res.render("roll", { title: "Student Relationship", courses });
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
 
-    if (courses) {
-      res.render('roll', { title: 'Student Relationship', courses })
-    }
-  } catch (error) {
-    console.log(error)
-  }
-})
+app.get("/courses", login, async (req, res) => {
+	const endpoint = `http://localhost:3000/courses`;
 
-app.get('/courses', login, async (req, res) => {
-  const endpoint = `http://localhost:3000/courses`
+	try {
+		const response = await fetch(endpoint);
+		const courses = await response.json();
 
-  try {
-    const response = await fetch(endpoint)
-    const courses = await response.json()
+		if (courses) {
+			res.render("courses", { title: "Courses", courses });
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
 
-    if (courses) {
-      res.render('courses', { title: 'Courses', courses })
-    }
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-app.listen(3001)
+app.listen(process.env.PORT || 3001);
